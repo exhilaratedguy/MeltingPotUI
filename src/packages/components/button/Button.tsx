358f80aed302a70356a21a6758c25c2fe493d715
Francisco Teixeira <32616250+exhilaratedguy@users.noreply.github.com>
2023-08-17 22:25:06 +0100

    Merging outside className on Button

--- src/packages/components/button/Button.tsx
+++ src/packages/components/button/Button.tsx
@@ -1,15 +1,17 @@
 import { PropsWithChildren } from "react";
 import { useButtonStyles } from "./Button.styles";
 import { ButtonProps } from "./ButtonInterfaces";
+import { cx } from "classix";
 
 const Button = ({
   children,
+  className,
   ...buttonProps
 }: PropsWithChildren<ButtonProps>) => {
   const classes = useButtonStyles({ disabled: buttonProps.disabled });
 
   return (
-    <button className={classes.button} {...buttonProps}>
+    <button className={cx(classes.button, className)} {...buttonProps}>
       <span className={classes.label}>{children}</span>
     </button>
   );
