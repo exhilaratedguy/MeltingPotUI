cfa33856e90d227d21e1a015104782ba380b8ef7
Francisco Teixeira <32616250+exhilaratedguy@users.noreply.github.com>
2023-09-03 01:36:51 +0100

    Changed simple btn styles

--- src/packages/components/button/SimpleButton/SimpleButton.styles.tsx
+++ src/packages/components/button/SimpleButton/SimpleButton.styles.tsx
@@ -4,9 +4,13 @@ import { ButtonTheme } from "../ButtonInterfaces";
 export const simpleButtonTheme = (
   theme: DefaultTheme
 ): ButtonTheme["simple"] => ({
+  label: {
+    color: theme.palette.greys.darker,
+  },
+
   hovered: {
     root: {
-      backgroundColor: theme.palette.greys.lighter,
+      backgroundColor: theme.palette.greys.lightest,
     },
   },
 });
